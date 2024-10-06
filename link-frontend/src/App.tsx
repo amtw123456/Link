4928fca88dc2cd42f739ad9ca7a8b39f89abf114
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-06 15:46:51 +0800

    Project Initialization: React in tsx with material UI and Tailwind

---
+++ link-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './components/card/Card';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold flex items-center justify-center">
+      <Card />
+    </h1>
+  );
+}
+
+export default App;
