321e0f024549eed651d878b61b0aad1f4840e974
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-07 23:08:12 +0800

    chore: refactored folder names

--- link-frontend/src/components/card/Card.tsx
+++ link-frontend/src/components/card/Card.tsx
@@ -53,7 +53,7 @@ export default function RecipeReviewCard() {
     };
 
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ width: '100%' }} >
             <CardHeader
                 avatar={
                     <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -106,19 +106,19 @@ export default function RecipeReviewCard() {
                     </Typography>
                     <Typography sx={{ marginBottom: 2 }}>
                         Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                        medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+                        medium-high heat. Add chicken, shrimp, and chorizo, and cook, stirring
                         occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
                         large plate and set aside, leaving chicken and chorizo in the pan. Add
-                        pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+                        pimentón, bay leaves, garlic, tomatoes, onion, salt, and pepper, and cook,
                         stirring often until thickened and fragrant, about 10 minutes. Add
                         saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
                     </Typography>
                     <Typography sx={{ marginBottom: 2 }}>
                         Add rice and stir very gently to distribute. Top with artichokes and
-                        peppers, and cook without stirring, until most of the liquid is absorbed,
+                        peppers, and cook without stirring until most of the liquid is absorbed,
                         15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
                         mussels, tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just tender, 5 to 7
+                        stirring until mussels have opened and rice is just tender, 5 to 7
                         minutes more. (Discard any mussels that don&apos;t open.)
                     </Typography>
                     <Typography>
@@ -126,6 +126,7 @@ export default function RecipeReviewCard() {
                     </Typography>
                 </CardContent>
             </Collapse>
-        </Card>
+        </ Card>
+
     );
 }
--- link-frontend/src/pages/homepage/Homepage.tsx
+++ link-frontend/src/pages/homepage/Homepage.tsx
@@ -1,7 +1,8 @@
 import { useState, ChangeEvent, FormEvent } from 'react';
 
-import AppBar from '../../components/appbar/AppBar';
-import RecipeReviewCard from '../../components/card/Card';
+import AppBar from '../../components/NavBar/NavBar';
+import RecipeReviewCard from '../../components/Card/Card';
+import Post from '../../components/Post/Post';
 
 
 const Homepage = () => {
@@ -9,12 +10,19 @@ const Homepage = () => {
         <>
             <AppBar />
             <div className="flex justify-center">
-                <div className="grid grid-cols-1 gap-4"> {/* Set a max-width for the grid container */}
+                <div className="w-1/3 flex justify-start">
+                    Red
+                </div>
+                <div className="w-1/3 flex flex-col justify-center items-center px-12">
+                    <Post />
                     <RecipeReviewCard />
                     <RecipeReviewCard />
                     <RecipeReviewCard />
                     <RecipeReviewCard />
                     <RecipeReviewCard />
+                </div>
+                <div className="w-1/3 flex justify-end">
+                    Red
                 </div>
             </div>
 
