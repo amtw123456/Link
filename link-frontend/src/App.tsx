c95b86a4db49c8e938f5506ef2232d2b131f9244
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-07 13:01:06 +0800

    chore: modified login and signup page text and made it more accurate

--- link-frontend/src/App.tsx
+++ link-frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+
 import Card from './components/card/Card';
+import { Routes, Route } from 'react-router-dom';
+
+import Signup from './pages/signup/Signup';
+import Signin from './pages/login/Login';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold flex items-center justify-center">
-      <Card />
-    </h1>
+    <>
+      <Routes>
+        <Route path="/login" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
