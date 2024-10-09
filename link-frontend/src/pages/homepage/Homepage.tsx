e76586dd0335841e956c10efb9a10e1289a04b82
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-09 20:48:06 +0800

    chore: some minor code refactors

--- link-frontend/src/pages/homepage/Homepage.tsx
+++ link-frontend/src/pages/homepage/Homepage.tsx
@@ -1,6 +1,6 @@
 import { useState, ChangeEvent, FormEvent } from 'react';
 
-import AppBar from '../../components/NavBar/NavBar';
+import NavBar from '../../components/NavBar/NavBar';
 import RecipeReviewCard from '../../components/PostCard/PostCard';
 import Post from '../../components/Post/Post';
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
@@ -11,10 +11,10 @@ import FollowCard from '../../components/FollowCard/FollowCard';
 const Homepage = () => {
     return (
         <>
-            <AppBar />
+            <NavBar />
             <div className="mx-auto max-w-7xl flex justify-center">
                 <div className='flex flex-row mt-8'>
-                    <div className="w-1/4 flex justify-start">
+                    <div className="w-1/4 justify-start">
                         <ProfileCard />
                     </div>
                     <div className="w-2/4 flex flex-col justify-center items-center px-8 space-y-8">
